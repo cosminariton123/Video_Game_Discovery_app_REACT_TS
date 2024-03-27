4dc8eae42b83c39dedef97f0480dab4e642c3893
cosminariton123 <cosmin.ariton@gmail.com>
2024-03-27 22:01:27 +0200

    removed unused imports

--- src/components/PlatformIconList/PlatformIconList.tsx
+++ src/components/PlatformIconList/PlatformIconList.tsx
@@ -9,7 +9,7 @@ import {
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
 import { Platform } from "../../hooks/useGames";
 import { IconType } from "react-icons";
 
