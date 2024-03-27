1835269595e0ae3a13f71ea5f7c2554b97eb66d2
cosminariton123 <cosmin.ariton@gmail.com>
2024-03-27 21:47:16 +0200

    create a generic data fetching hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,43 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
     id: number
     name: string
 }
 
-interface FetchGenresResponse {
-    count: number;
-    results: Genre[]
-}
-
-const useGenres = () => {
-    const [genres, setGenre] = useState<Genre[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
-  
-    useEffect(() => {
-      const controller = new AbortController();
-  
-      setLoading(true);
-      apiClient
-        .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-        .then((res) => {
-          setGenre(res.data.results);
-          setLoading(false);
-        })
-        .catch((err) => {
-          if (err instanceof CanceledError) return;
-          setError(err.message);
-          setLoading(false);
-        });
-      //.finally(setLoading(false)); But it doesn't work with strict mode one
-  
-      return () => controller.abort();
-    }, []);
-  
-    return { genres, error, isLoading };
-  };
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
