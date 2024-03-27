35d9a251a3b5da9cf9f1c7aa5ff53f32e4b0d76e
cosminariton123 <cosmin.ariton@gmail.com>
2024-03-27 16:46:47 +0200

    show_loading_skeletons

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,12 +1,4 @@
-import {
-  Card,
-  CardBody,
-  HStack,
-  Heading,
-  Image,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../../hooks/useGames";
 import PlatformIconList from "../PlatformIconList";
 import CriticScore from "../CriticScore";
@@ -18,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="300px" borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
