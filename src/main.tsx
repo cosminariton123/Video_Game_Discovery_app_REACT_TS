21beb20194739352b206cb02509af057007bd5e6
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-06 02:26:19 +0300

    Added React Query DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript
-          initialColorMode={theme.config.initialColorMode}
-        ></ColorModeScript>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript
+        initialColorMode={theme.config.initialColorMode}
+      ></ColorModeScript>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </ChakraProvider>
-    </QueryClientProvider>
+        <ReactQueryDevtools></ReactQueryDevtools>
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
