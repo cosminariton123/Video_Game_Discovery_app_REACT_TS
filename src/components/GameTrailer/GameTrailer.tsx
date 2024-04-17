5308a6c2848090bf40afcbd349dfd9271e4e7137
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-17 03:53:49 +0300

    Build Game trailer

---
+++ src/components/GameTrailer/GameTrailer.tsx
@@ -0,0 +1,20 @@
+import useTrailers from "../../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0];
+  if (!first) return null;
+
+  return <video src={first.data[480]} poster={first.preview} controls></video>;
+};
+
+export default GameTrailer;
