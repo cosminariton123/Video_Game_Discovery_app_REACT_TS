2062c56faf7bc7dd6fc98cc80c78d08b8e3a70bd
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-17 04:26:57 +0300

    Alligned Genres with navbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -18,7 +18,7 @@ const HomePage = () => {
       }}
     >
       <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
+        <GridItem area="aside">
           <GenreList></GenreList>
         </GridItem>
       </Show>
